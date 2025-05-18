e127f6cadc8055a2da52cbc4c3af8bf58f6cb162
Yuliana Mustika Sari <yulianamustikasari0@gmail.com>
2025-05-18 20:17:45 +0700

    mengedit halaman welcome-again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,5 +10,6 @@
     <p>Welcome again from views</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur dolorem iure nam ipsa eveniet ipsum, aut natus repellat sequi veniam perferendis necessitatibus, enim, incidunt illo? Accusantium illum iste rerum! Esse!</p>
     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, natus dolores. Rem molestias aperiam iste. Fugiat ipsam suscipit quo quia eveniet laboriosam, ratione vero ducimus maxime debitis impedit! Vitae, accusamus?</div>
+    <p>hiiii</p>
 </body>
 </html>
