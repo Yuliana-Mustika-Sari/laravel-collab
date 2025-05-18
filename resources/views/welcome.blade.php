44c9b40e7074447935cd44d420dbcad2c8837ebd
Yuliana Mustika Sari <yulianamustikasari0@gmail.com>
2025-05-18 19:55:25 +0700

    Admin mengubah text welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,5 +9,6 @@
     <h1>Welcome to Laravel</h1>
     <p>Welcome again from views</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur dolorem iure nam ipsa eveniet ipsum, aut natus repellat sequi veniam perferendis necessitatibus, enim, incidunt illo? Accusantium illum iste rerum! Esse!</p>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, natus dolores. Rem molestias aperiam iste. Fugiat ipsam suscipit quo quia eveniet laboriosam, ratione vero ducimus maxime debitis impedit! Vitae, accusamus?</div>
 </body>
 </html>
